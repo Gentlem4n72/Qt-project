f1e9f131180153c39ed274aa1e51d5fc2364c37d
Gentlem4n72 <98234641+Gentlem4n72@users.noreply.github.com>
2022-10-27 21:50:18 +0300

    Add files via upload

--- shop/shop.py
+++ shop/shop.py
@@ -1,7 +1,10 @@
-import sqlite3
 import sys
+import sqlite3
+import datetime as dt
+from docx import Document
+from docxtpl import DocxTemplate
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QFileDialog
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
 from PyQt5 import uic
 
@@ -21,11 +24,12 @@ class Homepage(QMainWindow):
         accounts = sqlite3.connect('shop_db.sqlite')
         cur = accounts.cursor()
         login, password = self.loginLine.text(), self.passwordLine.text()
-        result = cur.execute('''SELECT login, password FROM staff
+        result = cur.execute('''SELECT name, login, password FROM staff
                                     WHERE login = ? AND password = ?''', (login, password)).fetchone()
         accounts.close()
 
-        if (login, password) == result:
+        if (login, password) == result[1:]:
+            self.mainwindow.cashier = result[0]
             self.mainwindow.show()
             homepage.close()
         else:
@@ -42,9 +46,14 @@ class Mainwindow(QMainWindow):
 
         self.current_amount = 0
         self.current_sum = 0.0
-        self.display()
+        self.order = {}
+        self.overall_discount = 0
+        self.cashier = ''
+
+        self.fname = 'check.docx'
 
         self.connection = sqlite3.connect("shop_db.sqlite")
+        self.cursor = self.connection.cursor()
         self.search()
 
         self.newOrderButton.clicked.connect(self.new_order)
@@ -52,44 +61,79 @@ class Mainwindow(QMainWindow):
         self.formButton.clicked.connect(self.form)
         self.dbTableWidget.cellChanged.connect(self.current_amount_sum)
         self.searchLine.textChanged.connect(self.search)
+        self.saveFilePathButton.clicked.connect(self.select_path)
 
     def form(self):
-        pass
+        template = DocxTemplate('check_template.docx')
+        data = {
+            'items': ['    '.join(map(str, x[:2])) + '\n'
+                      + '                    '.join(map(str, x[2:]))
+                      for x in self.order.values()],
+            'discount': self.overall_discount,
+            'total': self.current_sum,
+            'cashier': self.cashier,
+            'date': dt.date.today(),
+            'time': dt.datetime.now().strftime("%H:%M")
+        }
+        template.render(data)
+        template.save(self.fname)
+
+        text = []
+        for par in Document(self.fname).paragraphs:
+            text.append(par.text)
+        self.checkPreviewText.setText('\n'.join(text))
+
+    def select_path(self):
+        self.fname = QFileDialog.getSaveFileName(self, 'Выбрать путь сохранения', 'check.docx')[0]
 
     def new_order(self):
-        pass
+        self.form()
+        self.cancel()
 
     def cancel(self):
-        pass
+        self.order = {}
+        self.overall_discount = 0
+        self.cursor.execute('''UPDATE items SET quantity = 0''')
+        self.searchLine.setText('')
+        self.search()
+        self.checkPreviewText.clear()
+        self.fname = 'check.docx'
 
     def current_amount_sum(self):
-        self.current_amount = 0
-        self.current_sum = 0.0
         for i in range(self.dbTableWidget.rowCount()):
-            if (self.dbTableWidget.item(i, 2) is None or
-                    self.dbTableWidget.item(i, 1) is None):
+            if self.dbTableWidget.item(i, 4) is None:
                 return
-            amount, price = int(self.dbTableWidget.item(i, 2).text()), float(self.dbTableWidget.item(i, 1).text())
-            self.current_amount += amount
-            self.current_sum += price * amount
-        self.display()
-
-    def display(self):
+            id, name, price, discount, quantity = [self.dbTableWidget.item(i, x).text() for x in range(5)]
+            id, name, price, discount, quantity = int(id), name.upper(), float(price), int(discount), int(quantity)
+            if quantity == 0 or (name in self.order and quantity == self.order[name][4]):
+                continue
+            total = round(quantity * price * (100 - discount) / 100, 2)
+            discount = '       ' if discount == 0 else str(discount) + '%'
+            self.order[name] = (id, name, price, discount, quantity, total)
+            self.overall_discount += round(quantity * price - total, 2)
+            self.cursor.execute('''UPDATE items
+                                    SET quantity = ?
+                                    WHERE id = ?''', (quantity, id))
+        self.current_amount = sum(x[1][4] for x in self.order.items())
+        self.current_sum = round(sum(x[1][5] for x in self.order.items()), 2)
         self.amountLine.setText(str(self.current_amount))
         self.resultSumLine.setText(str(self.current_sum))
 
     def search(self):
         request = f'%{self.searchLine.text()}%'
-        res = self.connection.cursor().execute('''SELECT name, price, quantity FROM items
+        res = self.cursor.execute('''SELECT * FROM items
                                                     WHERE name LIKE ?''', (request,)).fetchall()
 
-        self.dbTableWidget.setColumnCount(3)
+        self.dbTableWidget.setColumnCount(5)
         self.dbTableWidget.setRowCount(0)
 
-        self.dbTableWidget.setHorizontalHeaderLabels(('Название', 'Цена', 'Количество'))
+        self.dbTableWidget.setHorizontalHeaderLabels(('ID', 'Название', 'Цена', 'Cкидка', 'Количество'))
         horizontal_h = self.dbTableWidget.horizontalHeader()
-        horizontal_h.resizeSection(0, 274)
+        horizontal_h.resizeSection(0, 30)
+        horizontal_h.resizeSection(1, 265)
         horizontal_h.resizeSection(2, 80)
+        horizontal_h.resizeSection(3, 80)
+        horizontal_h.resizeSection(4, 80)
 
         for i, row in enumerate(res):
             self.dbTableWidget.setRowCount(
