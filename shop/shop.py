f1641c8fb6b1a8cce658cb5c216db1c3ad771fb9
Gentlem4n72 <98234641+Gentlem4n72@users.noreply.github.com>
2022-11-10 20:31:54 +0300

    Update shop.py

--- shop/shop.py
+++ shop/shop.py
@@ -4,8 +4,8 @@ import datetime as dt
 from docx import Document
 from docxtpl import DocxTemplate
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QFileDialog
-from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+from PyQt5.QtWidgets import QTableWidgetItem, QFileDialog, QDialog, QMessageBox, QInputDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import Qt
 from PyQt5 import uic
 
@@ -15,6 +15,7 @@ class Homepage(QMainWindow):
     def __init__(self):
         super(Homepage, self).__init__()
         uic.loadUi('UI/Homepage.ui', self)
+        self.setFixedSize(800, 500)
 
         self.enterButton.clicked.connect(self.enter)
         self.enterButton.setEnabled(False)
@@ -33,9 +34,12 @@ class Homepage(QMainWindow):
 
         if result is not None and (login, password) == result[2:]:
             mainwindow.cashier = result[0]
-            mainwindow.post = cur.execute('''SELECT title FROM posts
-                                                    WHERE postid = ?''', (result[1])).fetchone()[0]
+            mainwindow.edit_flag = cur.execute('''SELECT title FROM posts
+                                                    WHERE postid = ?''', (result[1])).fetchone()[0] == 'менеджер'
             mainwindow.show()
+            if not mainwindow.edit_flag:
+                mainwindow.addStaffButton.hide()
+                mainwindow.addItemButton.hide()
             mainwindow.search()
             homepage.close()
         else:
@@ -58,6 +62,7 @@ class Mainwindow(QMainWindow):
     def __init__(self):
         super(Mainwindow, self).__init__()
         uic.loadUi('UI/Mainwindow.ui', self)
+        self.setFixedSize(910, 670)
 
         self.current_amount = 0
         self.current_sum = 0.0
@@ -65,6 +70,7 @@ class Mainwindow(QMainWindow):
         self.overall_discount = 0
 
         self.fname = 'check.docx'
+        self.edit_flag = False
 
         self.connection = sqlite3.connect("db/shop_db.sqlite")
         self.cursor = self.connection.cursor()
@@ -75,6 +81,8 @@ class Mainwindow(QMainWindow):
         self.dbTableWidget.itemChanged.connect(self.current_amount_sum)
         self.searchLine.textChanged.connect(self.search)
         self.saveFilePathButton.clicked.connect(self.select_path)
+        self.addItemButton.clicked.connect(self.add_item)
+        self.addStaffButton.clicked.connect(self.add_staff)
 
     def form(self):
         #Формирует чек из заданных товаров в соответствии с шаблоном “check_template.docx”.
@@ -161,7 +169,7 @@ class Mainwindow(QMainWindow):
         id, name, price, discount = (self.dbTableWidget.item(item.row(), x).text() for x in range(4))
         self.cursor.execute('''UPDATE items
                                 SET name = ?, price = ?, discount = ?
-                                WHERE id = ?''', (name, price, discount, id))
+                                WHERE id = ?''', (name.lower(), price, discount, id))
         self.connection.commit()
         self.current_amount_sum(self.dbTableWidget.item(item.row(), 4))
 
@@ -179,22 +187,45 @@ class Mainwindow(QMainWindow):
         horizontal_h.resizeSection(0, 30)
         horizontal_h.resizeSection(1, 265)
         horizontal_h.resizeSection(2, 80)
-        horizontal_h.resizeSection(3, 80)
+        horizontal_h.resizeSection(3, 56)
         horizontal_h.resizeSection(4, 80)
-
-        # Флаг в связи с которым будет предоставляться возможность редактировать ячейки
-        edit_flag = self.post == 'Менеджер'
 
         for i, row in enumerate(res):
             self.dbTableWidget.setRowCount(
                 self.dbTableWidget.rowCount() + 1)
             for j, elem in enumerate(row):
                 self.dbTableWidget.setItem(
-                    i, j, QTableWidgetItem(str(elem)))
-                if j == 0 or (not edit_flag and j != 4):
+                    i, j, QTableWidgetItem(str(elem).capitalize()))
+                if j == 0 or (not self.edit_flag and j != 4):
                     #Колонка "ID" не подлежит изменению, а изменения колонки "Количество" доступно любому сотруднику
                     self.dbTableWidget.item(i, j).setFlags(
                         self.dbTableWidget.item(i, j).flags() ^ Qt.ItemIsEditable)
+    def add_item(self):
+        #Добавляет новый товар в базу данных
+        dialog = AddItemDialog()
+        if dialog.exec():
+            name, price, discount = dialog.nameLine.text(), dialog.priceLine.text(), dialog.discountLine.text()
+            self.cursor.execute('''INSERT INTO items(name, price, discount, quantity) VALUES(?, ?, ?, 0)''',
+                                (name, price, discount))
+            self.connection.commit()
+            self.search()
+
+    def add_staff(self):
+        #Добавляет нового сотрудника в базу данных
+        dialog = AddStaffDialog()
+        if dialog.exec():
+            name, post, login, password = (dialog.nameLine.text(), dialog.postComboBox.currentText(),
+                                           dialog.loginLine.text(), dialog.passwordLine.text())
+            post_id = self.cursor.execute('''SELECT postid FROM posts
+                                                                WHERE title = ?''', (post,)).fetchone()
+            if self.cursor.execute('''SELECT * FROM staff
+                                        WHERE login = ? AND password = ?''',
+                                   (login, password)).fetchone() is None:
+                self.cursor.execute('''INSERT INTO staff(name, post, login, password) VALUES(?, ?, ?, ?)''',
+                                    (name, *post_id, login, password))
+                self.connection.commit()
+            else:
+                QMessageBox.about(self, 'Ошибка', 'Сотрудник с таким паролем и логином уже существует')
 
     def closeEvent(self, event):
         #При закрытии приложения возвращаем колонке "Количество" значения 0, коммитим и закрываем базу данных
@@ -203,6 +234,24 @@ class Mainwindow(QMainWindow):
         self.connection.commit()
         self.connection.close()
 
+
+class AddStaffDialog(QDialog):
+    #Диалоговое окно добавления нового сотрудника
+    def __init__(self):
+        super(QDialog, self).__init__()
+        uic.loadUi('UI/AddStaffDialog.ui', self)
+        self.setWindowTitle('Добавление сотрудника')
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+class AddItemDialog(QDialog):
+    # Диалоговое окно добавления нового товара
+    def __init__(self):
+        super(QDialog, self).__init__()
+        uic.loadUi('UI/AddItemDialog.ui', self)
+        self.setWindowTitle('Добавление товара')
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
